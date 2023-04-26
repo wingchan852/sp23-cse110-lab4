1a113ea6cdfe331cd61733995fc81a244811f506
Wing <66709835+wingchan852@users.noreply.github.com>
2023-04-26 14:15:22 -0700

    Create part2-question16.js

---
+++ expose/javascript/part2-question16.js
@@ -0,0 +1,13 @@
+let statistics = {
+    redCars: 21,
+    blueCars: 45,
+    greenCars: 12,
+    raceCars: 5,
+    blackCars: 40,
+    rareCars: 2
+};
+
+for (const i in statistics) {
+    if (i[0] == 'r' || statistics[i] % 2 == 0)
+        console.log(`${statistics[i]}`);
+}
